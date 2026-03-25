a1eba32db05aa2ae5d75bb67296ddc464b17de19
Isadora Carvalho <117326019+Isadora-Correa@users.noreply.github.com>
2026-03-25 14:29:21 -0300

    config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
   logLevel: 'error', // Suppress warnings, only show errors
-  base: './',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
